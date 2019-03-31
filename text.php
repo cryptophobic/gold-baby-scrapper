2b5735a720f1e8ef124310e30edd416f12f3d425
voron1648 <medved.m13@gmail.com>
2019-03-31 21:20:43 +0300

    medved

--- text.php
+++ text.php
@@ -1,13 +1,23 @@
 <?php
 
-declare(strict_types = 1);
+include 'simplehtmldom/simple_html_dom.php';
 
-/**
- * @param int $a
- */
-function help(float $a)
-{
-    print_r($a);
+$dom = new \simple_html_dom();
+$fp = fopen('file_price.csv', 'w');
+fputcsv($fp, ['productId', 'Price', 'onStock']);
+
+for ($page = 1; $page <= 96; $page++) {
+    $dom->load_file('https://gold-baby.net/product_list/page_'.$page);
+    $goods = $dom->find('div.b-product-line_type_gallery');
+    var_dump($page);
+    foreach ($goods as $product) {
+        $id_product = 'data-product-id';
+        $productId = $product->$id_product;
+        $price = $product->children(2)->children(1)->children(0)->plaintext;
+        $onStock = $product->children(2)->children(2)->children(0)->plaintext;
+        fputcsv($fp, [$productId, $price, $onStock]);   
+        sleep(5);
+    }
 }
+fclose($fp);
 
-help(3/2);
